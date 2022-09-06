ee48c7ea34c41e4bb811489bc3ce2f44c0380131
Bas Nijholt <bas@nijho.lt>
2022-09-06 13:13:21 -0700

    Bump to 0.6

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="slurm-usage",
-    version="0.5",
+    version="0.6",
     description="Command to list the current cluster usage per user.",
     url="https://github.com/basnijholt/slurm-usage",
     author="Bas Nijholt",
